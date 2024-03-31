80975e7de011d3a0b7a87a10c9365ebe54ef3862
Edson Santana <134112431+edsonsantana1@users.noreply.github.com>
2024-03-31 13:09:26 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+// Desafio 2
+let titulo = document.querySelector ("h1");
+titulo.innerHTML = "hora do desafio";
+
+//desafio 3
+
+function verificarConsole() {
+console.log ("o botao foi clicado");
+}
+
+//desafio 4
+
+function verificarAlerta() {
+    alert ("Eu amo javaScript");
+}
+
+//desafio 5
+
+function verificarPrompt() {
+    let cidade = prompt ("Digie o nome de uma cidade do Brasil:");
+    alert (`eu estive em ${cidade} e fiquei muito feliz `);
+}
+
+//desafio 6
+
+function verificarSoma() {
+    let num1 = parseInt (prompt ("digite um numero")); 
+    let num2 = parseInt (prompt ("digite outro numero"));
+    let soma = num1+num2;
+    alert (`a soma entre ${num1} e ${num2} é igual ao ${soma}`);
+}
+
+// index.HTML 
+
+        
